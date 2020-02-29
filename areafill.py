3367920e24ed3e3680e4366848a248d0d202d6e4
JacobJustice <jacobjustice@gmail.com>
2020-02-29 14:56:46 -0500

    most updated version

--- areafill.py
+++ areafill.py
@@ -3,21 +3,26 @@ import numpy as np
 
 def get_closest_color(available_pixel, chosen_pixel):
     distances = []
-    
+    #min_distances = 9999999
+
     for key, value in available_pixel.items():
-        a1 = np.asarray(value)
+        a1 = flipredblue(np.asarray(value))
         c1 = np.asarray(chosen_pixel)
-        curr_dist = np.linalg.norm(a1 - c1)
+        curr_dist = (((a1[0]-c1[0])*0.3)**2 + ((a1[1]-c1[1])*0.59)**2 + ((a1[2]-c1[2])*0.11)**2)**.5
         distances += [curr_dist]
-        if(curr_dist <= min(distances)):
+        if(curr_dist == min(distances)):
             curr_key = key
+            #print(key,curr_dist,distances)
 
     return curr_key
+
+def flipredblue(pixel):
+    return [pixel[2],pixel[1],pixel[0]]
 
 #
 # generates lines that draws diagnol lines to fill adjacent spaces with their closest color
 #
-def generate_diagnol_fill(image, color_dict):
+def generate_diagnolfill(image, color_dict):
     print(color_dict)
 
     diag_list = list(range(len(image),0,-1))
@@ -55,7 +60,7 @@ def generate_diagnol_fill(image, color_dict):
 
     return lines
 
-colors = {'red':[255,0,0], 'green':[0,255,0], 'blue':[0,0,255],'magenta':[255,0,255], 'tomato':[255,99,71], 'lawn green':[124,252,0], 'yellow':[255,217,41]}
-img = cv2.imread("./flower.jpg")
-lines = generate_diagnol_fill(img,colors)
+#colors = {'red':[255,0,0], 'green':[0,255,0], 'blue':[0,0,255],'magenta':[255,0,255], 'tomato':[255,99,71], 'lawn green':[124,252,0], 'yellow':[255,217,41]}
+#img = cv2.imread("./flower.jpg")
+#lines = generate_diagnol_fill(img,colors)
 #print(lines)
